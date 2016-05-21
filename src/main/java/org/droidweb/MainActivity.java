7fa721392ed7e9299d2b1c1362f50e2311b42b70
Paul <staura@gmail.com>
2016-05-21 14:37:49 +0200

    minor changes

--- src/main/java/org/droidweb/MainActivity.java
+++ src/main/java/org/droidweb/MainActivity.java
@@ -7,42 +7,39 @@ import android.webkit.JavascriptInterface;
 import android.widget.Toast;
 import android.content.Context;
 
-public class HelloActivity extends Activity {
-    private WebView wv;
+public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hello_layout);
         
-        //get webview and enable js
-        wv = (WebView) findViewById(R.id.web_view);
-        wv.getSettings().setJavaScriptEnabled(true);
-        
-        //add interface
-        wv.addJavascriptInterface(new WebAppInterface(this), "android");//android is the keyword that will be exposed in js
-        
-        //load file
-        wv.loadUrl("file:///android_asset/test.html");
+        setContentView(R.layout.main_layout);
     }
 
     @Override
     public void onStart() {
         super.onStart();
-        // LocalTime currentTime = new LocalTime();
-        // TextView textView = (TextView) findViewById(R.id.text_view);
-        // textView.setText("The current local time is: " + currentTime);
+        
+        // Get webview and enable js
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        
+        // Add interface ('android' is the keyword that will be exposed in js)
+        webView.addJavascriptInterface(new WebAppInterface(this), "android"); 
+        
+        // Load html file
+        webView.loadUrl("file:///android_asset/index.html");
     }
     
     public class WebAppInterface {
         Context mContext;
 
-        /** Instantiate the interface and set the context */
+        // Instantiate the interface and set the context
         WebAppInterface(Context c) {
             mContext = c;
         }
 
-        /** Show a toast from the web page */
+        // Show a toast from the web page
         @JavascriptInterface
         public void showToast(String toast) {
             Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
